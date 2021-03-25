0adb4a13ddd28c6e6ecb1e08ec35921905c6693b
Travis297 <travis.witts@outlook.com>
2021-03-25 19:55:38 +1030

    adding user-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const reviewRoutes = require('./api/reviewRoutes');
 
 router.use('/users', userRoutes);
+router.use('/review', reviewRoutes);
+
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const businessRoutes = require('./businessRoutes');
-const reviewRoutes = require('./reviewRoutes');
 
+router.use('/api', apiRoutes)
 router.use('/', homeRoutes);
 router.use('/business', businessRoutes);
-router.use('/review', reviewRoutes);
+
 
 module.exports = router;
