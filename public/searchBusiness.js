339ed329c7861424a2d73984e28ce8a628165fd5
netsy001 <nettem.surendra@gmail.com>
2021-03-25 21:11:56 +1030

    getting input from search

--- public/searchBusiness.js
+++ public/searchBusiness.js
@@ -2,16 +2,7 @@ const businessR = async (event) => {
     event.preventDefault();
     const businessSearch = document.querySelector('#searchBusiness').value.trim();
     if (businessSearch) {
-      // const response = await fetch(`/api/search`, {
-      //   method: 'POST',
-      //   body: JSON.stringify({ businessSearch }),
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      // });
-  
-      // if (response.ok) {
-        document.location.replace('api/search/businessSearch');
+        document.location.replace(`/business/search/${businessSearch}`);
       } else {
         alert('Failed to create search');
       }
